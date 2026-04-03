a7368a13b6009908b113b249d2d17a41969e67f5
Komal Kalshetti <komalkalshetti989@gmail.com>
2026-04-03 15:57:25 +0530

    Added UserRepository with findByEmail method

--- src/main/java/com/zorvyn/finance/repository/UserRepository.java
+++ src/main/java/com/zorvyn/finance/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.zorvyn.finance.repository;
 
-public class UserRepository {
+import com.zorvyn.finance.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
