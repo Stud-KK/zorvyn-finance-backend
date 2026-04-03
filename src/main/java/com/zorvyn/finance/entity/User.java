42a12ddd8a3c1a6262ae18a0ba9297a56e467dd4
Komal Kalshetti <komalkalshetti989@gmail.com>
2026-04-03 15:36:44 +0530

    add changes to entity user

--- src/main/java/com/zorvyn/finance/entity/User.java
+++ src/main/java/com/zorvyn/finance/entity/User.java
@@ -1,4 +1,34 @@
 package com.zorvyn.finance.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
 }
