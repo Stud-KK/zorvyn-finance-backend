ec95f9972136c532b6226a7f85f50cce44423e0c
Komal Kalshetti <komalkalshetti989@gmail.com>
2026-04-03 16:06:02 +0530

    Implemented AuthService with basic authentication flow and user persistence

--- src/main/java/com/zorvyn/finance/controller/AuthController.java
+++ src/main/java/com/zorvyn/finance/controller/AuthController.java
@@ -1,4 +1,25 @@
 package com.zorvyn.finance.controller;
 
+import com.zorvyn.finance.dto.auth.LoginRequest;
+import com.zorvyn.finance.dto.auth.RegisterRequest;
+import com.zorvyn.finance.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
 }
--- src/main/java/com/zorvyn/finance/service/AuthService.java
+++ src/main/java/com/zorvyn/finance/service/AuthService.java
@@ -1,4 +1,44 @@
 package com.zorvyn.finance.service;
 
+import com.zorvyn.finance.dto.auth.LoginRequest;
+import com.zorvyn.finance.dto.auth.RegisterRequest;
+import com.zorvyn.finance.entity.Role;
+import com.zorvyn.finance.entity.Status;
+import com.zorvyn.finance.entity.User;
+import com.zorvyn.finance.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public String register(RegisterRequest request) {
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(request.getPassword()) // later encode
+                .role(Role.ROLE_VIEWER)
+                .status(Status.ACTIVE)
+                .build();
+
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
+
+    public String login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return "Login successful";
+    }
 }
