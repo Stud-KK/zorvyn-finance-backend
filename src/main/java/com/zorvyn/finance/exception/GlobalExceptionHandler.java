71c5658d792b09f177c794fc8684468df5b36135
Komal Kalshetti <komalkalshetti989@gmail.com>
2026-04-06 11:34:10 +0530

    Implemented global exception handling for validation and runtime errors

--- src/main/java/com/zorvyn/finance/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zorvyn/finance/exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.zorvyn.finance.exception;
 
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.*;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(Map.of(
+                "error", ex.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(err ->
+                errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
